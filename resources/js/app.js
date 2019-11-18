99ab364ef7da1a9bd40c03ddc7108afbddbf7f25
Vitor Simões <vitor6427126@gmail.com>
2019-11-18 04:17:25 -0400

    Cifrando e descrifrando com 3DES

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,10 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
 require('./bootstrap');
+import Vue from 'vue';
+import axios from 'axios';
 
-window.Vue = require('vue');
 Vue.component('chat-messages', require('./components/ChatMessages.vue').default);
 Vue.component('chat-form', require('./components/ChatForm.vue').default);
 
@@ -21,7 +21,7 @@ Vue.component('chat-form', require('./components/ChatForm.vue').default);
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,19 +29,25 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+const NodeRSA = require('node-rsa');
+const CryptoJS = require("crypto-js");
+const key = new NodeRSA();
+
 const app = new Vue({
     el: '#app',
-
     data: {
-        messages: []
+        messages: [], keyDES: "",
     },
 
     created() {
+        key.generateKeyPair();
+        key.setOptions('sha1');
+        this.sendKey(key.exportKey('public'));
         this.fetchMessages();
         Echo.private('chat')
             .listen('MessageSent', (e) => {
                 this.messages.push({
-                    message: e.message.message,
+                    message:CryptoJS.TripleDES.decrypt(e.message.message, this.keyDES).toString(CryptoJS.enc.Utf8),
                     user: e.user
                 });
             });
@@ -50,14 +56,26 @@ const app = new Vue({
     methods: {
         fetchMessages() {
             axios.get('/messages').then(response => {
+                response.data.map(item => {
+                    item.message=CryptoJS.TripleDES.decrypt(item.message, this.keyDES).toString(CryptoJS.enc.Utf8);
+                });
                 this.messages = response.data;
             });
         },
 
-        addMessage(message) {
-            this.messages.push(message);
+        sendKey(key_public) {
+            let key2 = {'public_key': key_public};
+            axios.post('/key', key2).then(response => {
+                this.keyDES = key.decrypt(response.data, 'utf8');
+            });
+        },
 
-            axios.post('/messages', message).then(response => {
+        addMessage(message) {
+            console.log(message);
+            this.messages.push(message);
+            var ciphertext = CryptoJS.TripleDES.encrypt(message.message, this.keyDES);
+            var  mes={'mes':ciphertext.toString()};
+            axios.post('/messages', mes).then(response => {
                 console.log(response.data);
             });
         }
--- resources/js/components/ChatForm.vue
+++ resources/js/components/ChatForm.vue
@@ -1,7 +1,7 @@
-
 <template>
     <div class="input-group">
-        <input id="btn-input" type="text" name="message" class="form-control input-sm" placeholder="Type your message here..." v-model="newMessage" @keyup.enter="sendMessage">
+        <input id="btn-input" type="text" name="message" class="form-control input-sm"
+               placeholder="Type your message here..." v-model="newMessage" @keyup.enter="sendMessage">
 
         <span class="input-group-btn">
             <button class="btn btn-primary btn-sm" id="btn-chat" @click="sendMessage">
@@ -24,8 +24,7 @@
         methods: {
             sendMessage() {
                 this.$emit('messagesent', {
-                    user: this.user,
-                    message: this.newMessage
+                    user: this.user, message: this.newMessage
                 });
 
                 this.newMessage = ''
